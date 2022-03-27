68e9b3093e138af3b8e364e68b01ca9e7a1fc3e6
THANATATH <thanatuth.euro@gmail.com>
2022-03-28 04:47:15 +0700

    9.2

--- Activity#9/main.py
+++ Activity#9/main.py
@@ -8,6 +8,7 @@ from scipy import spatial
 from mpl_toolkits.axes_grid1 import ImageGrid
 from keras.preprocessing.image import load_img
 from skimage.measure import label
+from sklearn.cluster import KMeans
  
 
 all_files = glob.glob('./Animals/*/*.jpg')
@@ -30,5 +31,69 @@ ax[3].imshow(hog_image4, cmap=plt.cm.gray)
 plt.show()
 
 #################################### 9.1.2 ####################################################
+
+img = cv2.imread(all_files[0])
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+
+
+def hog_feature(img,Block_size):
+    Stride = (10, 10)
+    h, w, c = img.shape
+    tile_size = [50,50]
+    posRow = np.arange(0, h-Block_size[0]+1, Stride[0])
+    posCol = np.arange(0, w-tile_size[1]+1, Stride[1])
+    sub_img = []
+    for i in posRow:
+        for j in posCol:
+            sub_img_pre = img[i:i+Block_size[0], j:j+tile_size[1], :]
+            blur = cv2.GaussianBlur(sub_img_pre, (5, 5), 0)
+            fd, hog_image = hog(blur, orientations=9, pixels_per_cell=(4, 4),cells_per_block=(2, 2), visualize=True, multichannel=True)
+            sub_img.append(fd)
+    return sub_img
+
+
+hog_feature1 = hog_feature(img,(40, 20))
+hog_feature2 = hog_feature(img,(60, 20))
+hog_feature3 = hog_feature(img,(40, 30))
+    
+plot, ax = plt.subplots(1, 3, figsize=(12, 4), sharey=True, sharex=True)
+ax[0].imshow(hog_feature1, cmap=plt.cm.gray)
+ax[0].set_title('Block_size = (40, 20)')
+ax[1].imshow(hog_feature2, cmap=plt.cm.gray)
+ax[1].set_title('Block_size = (60, 20)')
+ax[2].imshow(hog_feature3, cmap=plt.cm.gray)
+ax[2].set_title('Block_size = (40, 30)')
+plt.show()
+
+
+#################################### 9.2 ####################################################
+
+#clean feature if NA
+
+def clean_feature_if_NA(feature):
+    for i in range(len(feature)):
+        if np.isnan(feature[i]):
+            feature[i] = 0
+    return feature
+
+
+hog_feature1_cleaned = clean_feature(hog_feature1)
+
+Kmean = KMeans(n_clusters=2, random_state=0)
+cluster = Kmean.fit_predict(hog_feature1_cleaned)
+
+def clusterID_to_grid(cluster):
+    grid = np.zeros((10, 50))
+    for i in range(len(cluster)):
+        grid[i//50, i%50] = cluster[i]
+    return grid
+
+cluster_array = clusterID_to_grid(cluster)
+
+print(cluster_array)
+plt.imshow(cluster_array, cmap='plasma')
+
 
 
