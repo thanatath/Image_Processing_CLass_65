60285b560ebc6315022ed9a7b86b55cdc7571394
THANATATH <thanatuth.euro@gmail.com>
2022-04-05 11:04:26 +0700

    update act 10

--- Activity#10/main.py
+++ Activity#10/main.py
@@ -7,6 +7,7 @@ from keras.layers import Dense,GlobalAveragePooling2D
 from keras.applications.mobilenet import MobileNet
 from keras.applications.mobilenet import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
 import pandas as pd
@@ -25,16 +26,18 @@ x = GlobalAveragePooling2D()(x)
 x = Dense(1024, activation='relu')(x)
 x = Dense(1024, activation='relu')(x)
 x = Dense(512, activation='relu')(x)
-preds = Dense(2, activation='softmax')(x)
+preds = Dense(3, activation='softmax')(x)
 
 #Assign transfer learning model to new model
 
 model = Model(inputs=base_model.input, outputs=preds)
 model.summary() #Before freezing
 
-for layer in model.layers[:20]:
+for layer in model.layers[:5]:
+    layer.trainable = True
+for layer in model.layers[8:10]:
     layer.trainable = False
-for layer in model.layers[20:]:
+for layer in model.layers[20:-1]:
     layer.trainable = True
 
 model.summary() #After freezing
@@ -49,17 +52,17 @@ datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2,rotation_range
 
 #Generate training and validation data
 
-train_generator = datagen.flow_from_directory( './train/', target_size=(224,224), batch_size=16, class_mode='categorical',color_mode='rgb' , seed=seed_value,shuffle=True)
+train_generator = datagen.flow_from_directory( './Cat_Dog_Horse/train/', target_size=(224,224), batch_size=32, class_mode='categorical',color_mode='rgb' , seed=seed_value,shuffle=True)
 
-val_generator = datagen.flow_from_directory( './valid/', target_size=(224,224), batch_size=16, class_mode='categorical',color_mode='rgb' , seed=seed_value,shuffle=True)
+val_generator = datagen.flow_from_directory( './Cat_Dog_Horse/validate/', target_size=(224,224), batch_size=16, class_mode='categorical',color_mode='rgb' , seed=seed_value,shuffle=True)
 
 def preview_image(datagen):
     x,y = datagen.next()
     plt.figure(figsize=(10,10))
-    for j in range(0,16):
+    for j in range(0,8):
         plt.subplot(4,4,j+1)
         plt.imshow(x[j])
-        plt.title(str(datagen.directory).split('/')[1])
+        plt.title(str(datagen.directory).split('/')[2])
         plt.axis('off')
     plt.show()
 
@@ -87,7 +90,7 @@ print(step_size_val)
 
 #train the model
 
-EP = 20
+EP = 50
 
 history = model.fit_generator(train_generator, steps_per_epoch=step_size_train, epochs=EP, validation_data=val_generator, validation_steps=step_size_val)
 
@@ -118,7 +121,7 @@ performanc_plot_loss(history)
 #create TEST data generator and predict
 
 test_data_generator = ImageDataGenerator(rescale=1./255)
-test_generator = test_data_generator.flow_from_directory( './test/', target_size=(224,224), batch_size=1, class_mode='categorical',color_mode='rgb',shuffle=False)
+test_generator = test_data_generator.flow_from_directory( './Cat_Dog_Horse/test/', target_size=(224,224), batch_size=1, class_mode='categorical',color_mode='rgb',shuffle=False)
 
 #class id
 y_true = test_generator.classes
@@ -133,7 +136,7 @@ for i in range(len(y_true)):
     pred_prob.append(np.array(pred))
     
 #prediction result
-pred_prob = np.array(pred_prob).reshape(len(y_true),2) #reshape to 2 columns
+pred_prob = np.array(pred_prob).reshape(len(y_true),3)
 df_pred = pd.DataFrame(pred_prob)
 df_class = df_pred.idxmax(axis=1)
 
@@ -141,5 +144,7 @@ df_class = df_pred.idxmax(axis=1)
 y_real = y_true
 y_pred = model.predict_generator(test_generator, steps=len(test_generator)).argmax(axis=-1)
 target_name = test_generator.class_indices
-print(confusion_matrix(y_real, y_pred))
+print(confusion_matrix(y_real, y_pred),'\n')
+print('REAL :\t \t',y_real)
+print('PREDICT :\t',y_pred)
 print(classification_report(y_real, y_pred, target_names=target_name))
